6d0cf38eaf1ffe09390959dc2b5354ca9fba2f33
divyadp <divyaparvathe_cb@discovery.com>
2024-01-26 10:31:57 +0100

    patient-hub-demo-v2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table PATIENT(
+        id int auto_increment,
+        patient_name varchar not null ,
+        date_Of_Birth date not null,
+        address varchar,
+        gender varchar
+);
